4b1bf2ab4fbabaa82cf638541d47fa2a8698b4ee
檠莲焰 <52445080+qinglianyan@users.noreply.github.com>
2020-11-03 19:59:27 +0800

    啊啊 有两个空没填QAQ

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -431,7 +431,9 @@ public final class Analyser {
             // 是标识符
 
             // 加载标识符的值
-            String name = /* 快填 */ null;
+        	Token a=expect(TokenType.Ident);
+        	
+            String name =(String)a.getValue();
             var symbol = symbolTable.get(name);
             if (symbol == null) {
                 // 没有这个标识符
@@ -446,6 +448,8 @@ public final class Analyser {
             // 是整数
             // 加载整数值
             int value = 0;
+            Token a=expect(TokenType.Uint);
+            value=(int)a.getValue();
             instructions.add(new Instruction(Operation.LIT, value));
         } else if (check(TokenType.LParen)) {
             // 是表达式
